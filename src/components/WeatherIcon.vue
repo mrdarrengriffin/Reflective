5c567244d3159747f06309878a9fa6c9090f4314
Darren Griffin <darren.griffin@live.co.uk>
2023-11-19 15:23:12 +0000

    Created Vue app

---
+++ src/components/WeatherIcon.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+defineProps<{
+  icon: string
+}>()
+</script>
+
+<template>
+    <img :src="`https://github.com/mrdarrengriffin/google-weather-icons/raw/main/v4/${icon}.svg`" alt="Weather icon">
+</template>
+<style scoped lang="scss">
+img {
+  width: 64px;
+  height: 100%;
+  aspect-ratio: 1/1;
+  display: flex;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import WeatherIcon from '@/components/WeatherIcon.vue';
+</script>
+
+<template>
+  <main>
+    <WeatherIcon icon="mostly_cloudy_day" />
+  </main>
+</template>
